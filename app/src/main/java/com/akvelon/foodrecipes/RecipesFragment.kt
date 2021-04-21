384e005fe579ecf54243f5ca9f798f2016030fd0
aiserrock <aicserrock@gmail.com>
2021-04-22 00:00:00 +0300

    added placeholder for recipes_row_layout and shimmer effect

--- app/src/main/java/com/akvelon/foodrecipes/RecipesFragment.kt
+++ app/src/main/java/com/akvelon/foodrecipes/RecipesFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import kotlinx.android.synthetic.main.fragment_recepies.view.*
 
 class RecipesFragment : Fragment() {
     override fun onCreateView(
@@ -12,6 +13,10 @@ class RecipesFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_recepies, container, false)
+        val view = inflater.inflate(R.layout.fragment_recepies, container, false)
+
+        view.recyclerview.showShimmer()
+
+        return view
     }
 }
